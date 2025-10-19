4e6ffb3f5cf122150443860c5dd337fd2f5a70f9
Hyeonsu KIM <wshyhs@gmail.com>
2025-10-19 21:09:49 +0900

    Feat : GlobalExceptionHandling
    회원가입 및 로그인에 관한 예외처리

---
+++ bank/src/main/java/com/baas/bank/account/exception/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.baas.bank.account.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
