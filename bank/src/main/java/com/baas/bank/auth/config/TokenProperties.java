cde2a4711c320c93f307b10436d9db9d3e5e0e6e
Hyeonsu KIM <wshyhs@gmail.com>
2025-10-20 17:44:27 +0900

    Feat: add JWT configuration and token provider
    SecurityConfig 설정
    JWT AccessToken, RefreshToken 생성 및 save

---
+++ bank/src/main/java/com/baas/bank/auth/config/TokenProperties.java
@@ -0,0 +1,23 @@
+package com.baas.bank.auth.config;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+public class TokenProperties {
+    private TokenConfig accessToken;
+    private TokenConfig refreshToken;
+
+    @Setter
+    @Getter
+    public static class TokenConfig {
+        private String name;
+        private long expiry;
+    }
+}
---
+++ bank/src/main/java/com/baas/bank/auth/service/JwtService.java
@@ -0,0 +1,32 @@
+package com.baas.bank.auth.service;
+
+import com.baas.bank.auth.config.TokenProperties;
+import com.baas.bank.auth.dao.RefreshDAO;
+import com.baas.bank.auth.entity.RefreshToken;
+import jakarta.servlet.http.Cookie;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+
+@Component
+@RequiredArgsConstructor
+public class JwtService {
+    private final TokenProperties tokenProperties;
+    private final RefreshDAO refreshDAO;
+
+    @Transactional
+    public boolean saveRefreshToken(Long userId, String refreshToken, long expiredMs) {
+        long expireAtMs = System.currentTimeMillis() + expiredMs;
+        RefreshToken entity = new RefreshToken(userId, refreshToken, String.valueOf(expireAtMs));
+        return refreshDAO.save(entity);
+    }
+
+    public Cookie createRefreshCookie(String token) {
+        Cookie cookie = new Cookie(tokenProperties.getRefreshToken().getName(), token);
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(30 * 60);
+        return cookie;
+    }
+}
