d28df1e5d9e7371e7848cbf97c79365097c3fce0
leeSH <dlfyd05680@gmail.com>
2025-10-20 23:51:30 +0900

    Add: RegisterDto 클래스 생성
    Register 전송 객체 생성

--- bank/src/main/java/com/baas/bank/oauth2/dto/RegisterDto.java
+++ bank/src/main/java/com/baas/bank/oauth2/dto/RegisterDto.java
@@ -3,22 +3,13 @@ package com.baas.bank.oauth2.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.Instant;
-
 @Getter
 @Setter
-public class RegisterDto {
-    private String id;
-    private String clientId;
-    private Instant clientIdIssuedAt;
-    private String clientSecret;
-    private Instant clientSecretExpiresAt;
+public class RegisterDTO {
+
     private String clientName;
-    private String clientAuthenticationMethods;
-    private String authorizationGrantTypes;
+    private String clientSecret;
     private String redirectUris;
     private String postLogoutRedirectUris;
     private String scopes;
-    private String clientSettings;
-    private String tokenSettings;
 }
