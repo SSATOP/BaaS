1762614d260583f1c5a23fdc5a2a0a32b40c525e
KibeomGwon <xtcc02332@naver.com>
2025-10-25 14:47:57 +0900

    Add: SEC_03 테스트 유저 생성 클래스추가 jwt 인증을 위한 테스트 유저 생성 클래스 추가 user2@gmail.com 을 이메일로 가지는 유저와 user1@gmail.com 을 이메일로 갖는 유저 초기 생성 각각의 유저의 jwt 토큰 생성

--- securities/src/main/java/com/baas/securities/config/SecurityConfig.java
+++ securities/src/main/java/com/baas/securities/config/SecurityConfig.java
@@ -3,18 +3,14 @@ package com.baas.securities.config;
 
 import com.baas.securities.security.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @RequiredArgsConstructor
 @Configuration
--- securities/src/main/java/com/baas/securities/dto/JwtDto.java
+++ securities/src/main/java/com/baas/securities/dto/JwtDto.java
@@ -7,6 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
 public class JwtDto {
     private String accessToken;
     private String refreshToken;
