a9cdcb0646f2e3f62be8875f1459b9f718a8542d
Hyeonsu KIM <wshyhs@gmail.com>
2025-11-12 00:29:28 +0900

    Feat: 사용자 정보 응답용 DTO(ResDTO, ResWrapper) 클래스 추가

---
+++ oauth_resourceServer/src/main/java/com/example/spring_oauth2_resource_server/dto/ResDTO.java
@@ -0,0 +1,14 @@
+package com.example.spring_oauth2_resource_server.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResDTO {
+
+    private String username;
+    private String gender;
+    private String email;
+    private String phoneNumber;
+}
