8bc6c1200eea1ecbff387ed7732d0ca4e3f22142
Hyeonsu KIM <wshyhs@gmail.com>
2025-11-12 00:26:08 +0900

    CREATE: start project

---
+++ oauth_resourceServer/src/main/java/com/example/spring_oauth2_resource_server/oauth_ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_oauth2_resource_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class oauth_ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(oauth_ResourceServerApplication.class, args);
+    }
+
+}
